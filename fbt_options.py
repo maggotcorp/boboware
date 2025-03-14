1e3c14d198ec949b578757866abc34792bb9b70c
timo <timotheuzi@yahoo.com>
2025-03-13 19:08:44 -0500

    update

--- fbt_options.py
+++ fbt_options.py
@@ -36,7 +36,7 @@ if not os.environ.get("DIST_SUFFIX"):
         # If not a tag, dist name is: mntm-(branch)-(commmit)
         branch_name = git("rev-parse", "--abbrev-ref", "HEAD").removeprefix("bobo-")
         commit_sha = git("rev-parse", "HEAD")[:8]
-        DIST_SUFFIX = f"mntm-{branch_name}-{commit_sha}"
+        DIST_SUFFIX = f"bobo-{branch_name}-{commit_sha}"
     # Dist name is only for naming of output files
     DIST_SUFFIX = DIST_SUFFIX.replace("/", "-")
     # Instead, FW version uses tag name (mntm-xxx), or "mntm-dev" if not a tag (see scripts/version.py)
