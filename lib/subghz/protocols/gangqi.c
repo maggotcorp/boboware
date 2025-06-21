69328f01206f871c028ca0e98d089ed7395e36cd
some guy <timotheuzi@yahoo.com>
2025-06-21 02:50:36 -0500

    uooop

--- lib/subghz/protocols/gangqi.c
+++ lib/subghz/protocols/gangqi.c
@@ -65,7 +65,7 @@ const SubGhzProtocol subghz_protocol_gangqi = {
     .type = SubGhzProtocolTypeStatic,
     .flag = SubGhzProtocolFlag_433 | SubGhzProtocolFlag_AM | SubGhzProtocolFlag_Decodable |
             SubGhzProtocolFlag_Load | SubGhzProtocolFlag_Save | SubGhzProtocolFlag_Send |
-            SubGhzProtocolFlag_Alarms,
+            SubGhzProtocolFilter_Alarms,
 
     .decoder = &subghz_protocol_gangqi_decoder,
     .encoder = &subghz_protocol_gangqi_encoder,
