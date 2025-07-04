2f4342f34d137f0c344472a8bfa3aa313d6bfd52
some guy <timotheuzi@yahoo.com>
2025-07-04 18:38:17 -0500

    uooop

--- applications/main/subghz/helpers/subghz_frequency_analyzer_worker.h
+++ applications/main/subghz/helpers/subghz_frequency_analyzer_worker.h
@@ -46,9 +46,9 @@ void subghz_frequency_analyzer_worker_set_pair_callback(
  * 
  * @param instance SubGhzFrequencyAnalyzerWorker instance
  * @param txrx pointer to SubGhzTxRx
- */
-void subghz_frequency_analyzer_worker_start(SubGhzFrequencyAnalyzerWorker* instance, SubGhzTxRx* txrx);
 
+void subghz_frequency_analyzer_worker_start(SubGhzFrequencyAnalyzerWorker* instance, SubGhzTxRx* txrx);
+*/
 /** Stop SubGhzFrequencyAnalyzerWorker
  * 
  * @param instance SubGhzFrequencyAnalyzerWorker instance
